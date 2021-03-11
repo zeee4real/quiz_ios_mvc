adb92938dd0906151b85d2a409929523b1e3269b
Asadulla Zaid <asadulla.mir@honeywell.com>
2021-03-11 17:37:20 +0530

    first commit

---
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+   
+    
+    var questionNumber = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUi()
+    }
+    
+    @objc func updateUi(){
+        questionLabel.text = quiz[questionNumber].text
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+        progressBar.progress = Float(questionNumber + 1) / Float(quiz.count)
+    }
+    
+    var timer = Timer()
+
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[questionNumber].answer
+        
+        if userAnswer == actualAnswer {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+        
+        if questionNumber + 1 < quiz.count {
+            questionNumber += 1
+        } else {
+            questionNumber = 0
+        }
+        
+        timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUi), userInfo: nil, repeats: false)
+      
+    }
+    
+}
+
---
+++ Quizzler-iOS13/Model/QuizBrain.swift
@@ -0,0 +1,9 @@
+//
+//  QuizBrain.swift
+//  Quizzler-iOS13
+//
+//  Created by H349930 on 11/03/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
